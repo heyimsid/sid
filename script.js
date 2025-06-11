8936bb9f3e78ee4ce63cdc901491f00c5180bd70
Sid <152196280+heyimsid@users.noreply.github.com>
2025-06-11 22:49:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -97,19 +97,13 @@
     updateColors(e.target.value);
   });
 
-  // 3D tilt effect on graphic wrapper
-  graphicWrapper.addEventListener('mousemove', (e) => {
-    const rect = graphicWrapper.getBoundingClientRect();
-    const x = e.clientX - rect.left;  
-    const y = e.clientY - rect.top;   
-    const centerX = rect.width / 2;
-    const centerY = rect.height / 2;
-    const rotateX = ((y - centerY) / centerY) * 10;
-    const rotateY = ((x - centerX) / centerX) * 10;
-    graphicWrapper.style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg)`;
-    profileImage.style.transform = `translate(-50%, -50%) rotateX(${-rotateX}deg) rotateY(${rotateY}deg)`;
+  // 3D tilt effect on graphic wrapper - simplified for 3D hover rotation
+  graphicWrapper.addEventListener('mouseenter', () => {
+    graphicWrapper.classList.add('tilt');
   });
+
   graphicWrapper.addEventListener('mouseleave', () => {
+    graphicWrapper.classList.remove('tilt');
     graphicWrapper.style.transform = `rotateX(0deg) rotateY(0deg)`;
     profileImage.style.transform = `translate(-50%, -50%) rotateX(0deg) rotateY(0deg)`;
   });
